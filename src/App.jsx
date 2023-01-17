9f7b25dea33fc60539106a5a41198ba85896257b
Gabriel Formigoni <bielformigoni@gmail.com>
2023-01-17 16:48:54 -0300

    Feat: Estrutura do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+
 import './App.css'
 
 function App() {
 
   return (
     <div className="">
-
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
     </div>
   )
 }
