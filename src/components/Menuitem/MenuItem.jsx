242a0d29dd191257dbd7189c902920b3554c2a9a
Gabriel Formigoni <bielformigoni@gmail.com>
2023-01-19 20:45:44 -0300

    Feat: Menu section

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({ title, description, price}) => (
+  <div className='app__menuItem'>
+    <div className='app__menuItem-title'>
+      <h1 className='p__cormorant'>{title}</h1>
+      <div className='app__menuItem-title_price'>
+        <div />
+        <p className='p__cormorant'>{price}</p>
+      </div>
+    </div>
+    <div className='app__menuItem-description'>
+      <p className='p__opensans'>{description}</p>
+    </div>
   </div>
 );
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { MenuItem, SubHeading } from '../../components';
 
+import { images, data } from "../../constants"
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className='app__menu flex__center section__padding' id='menu'>
+    <div className='app__menu-header'>
+      <SubHeading title="Menu That Fits Your Palatte"/>
+      <h1 className='headtext__cormorant'>Today's Special</h1>
+    </div>
+    <div className='app__menu-content'>
+      <div className='app__menu-content_left'>
+        <h1 className='app__menu-content_left-h1'>Wine & Beer</h1>
+        {data.wines.map((wine, idx) => (
+          <MenuItem title={wine.title} description={wine.tags} price={wine.price} key={idx}/>
+        ))}
+      </div>
+      <div className='app__menu-content_img'>
+        <img src={images.menu} alt="" />
+      </div>
+      <div className='app__menu-content_right'>
+        <h1 className='app__menu-content_right-h1'>Cocktails</h1>
+        {data.cocktails.map((cocktail, idx) => (
+          <MenuItem title={cocktail.title} description={cocktail.tags} price={cocktail.price} key={idx}/>
+        ))}
+      </div>
+    </div>
+    <button className='custom__button'>View More</button>
   </div>
 );
 
